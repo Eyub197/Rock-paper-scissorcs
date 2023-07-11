e62946a6a2920f46ab5bf9dfef9dbc99a76db99c
Eyub197 <eyubmehmed122@gmail.com>
2023-07-11 13:18:24 +0300

    Fixed sily syntax and now the playUntilYouExit is working

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const choice = ["rock", "paper", "scissors"]
-let playerChoice = prompt("Choose rock, paper or scissors").toLowerCase
 
 let getComputerChoice = () => {
     let randomIndex = Math.floor(Math.random() * choice.length) 
@@ -7,15 +6,19 @@ let getComputerChoice = () => {
     return computerChoice
 }
 
+let getPlayerChoice = () => {
+    let choice = prompt("Choose rock paper or scissors :")
+    return choice
+}
 
-let rules = (computerChoice = getComputerChoice() , plChoice = playerChoice) => {
+let rules = (computerChoice = getComputerChoice() , playerChoice = getPlayerChoice()) => {
     let message
     if(
-    (computerChoice === "rock" && plChoice === "scissors") || 
-    (computerChoice === "paper" &&  plChoice === "rock")   ||
-    (computerChoice === "scissors" && plChoice === "paper")){
+    (computerChoice === "rock" && playerChoice === "scissors") || 
+    (computerChoice === "paper" &&  playerChoice === "rock")   ||
+    (computerChoice === "scissors" && playerChoice === "paper")){
         message = "You lost"
-    } else if (computerChoice === plChoice){
+    } else if (computerChoice === playerChoice){
         message = "Tie"
     }else{
         message = "You win"
@@ -23,15 +26,61 @@ let rules = (computerChoice = getComputerChoice() , plChoice = playerChoice) => 
     return message
 }
 
-let determinateWinner = (computerChoice = getComputerChoice, plChoice = playerChoice) => {
-    let result = rules(computerChoice, plChoice)
+let determinateWinner = (computerChoice = getComputerChoice(), playerChoice = getPlayerChoice()) => {
+    let result = rules(computerChoice, playerChoice)
     let winner
-    if(result = "You lost"){
+
+    if(result === "You lost"){
         winner = "computer"
-    }else if (result = "Tie"){
-         winner = "tie"}
+        return winner
+    }else if (result === "Tie"){
+         winner = "tie"
+         return winner}
     else{
-        winner = "you"}
-    return winner
+        winner = "you"
+        return winner
+    }
+
+    
 }
 
+let playUntilYouExit = () => {
+    console.log("Welcome to my game, this mode you will play until you enter exit")
+
+    let computerWins = 0
+    let playerWins = 0
+    let tie = 0
+    let score = `Wins: ${playerWins} ties: ${tie} loses: ${computerWins}`
+    let input = prompt("Enter rock paper scissors or 'exit': ")
+
+    while(input != "exit"){
+        if(input === "exit"){break}
+
+        let computerChoice = getComputerChoice();
+        let playerChoice = input;
+        let winner = determinateWinner(computerChoice, playerChoice)
+
+        if(winner === "computer"){
+            computerWins++
+            console.log(`The computer won ${computerChoice} beats ${playerChoice}`)
+        } else if(winner === "tie"){
+            tie++
+            console.log(`TIE`)
+            console.log(score)
+        } else {
+            playerWins++
+            console.log(`The player won ${playerChoice} beats ${computerChoice}`)
+        }
+
+        score = `Wins: ${playerWins} ties: ${tie} loses: ${computerWins}`
+        console.log(score)
+
+       input = prompt("Enter rock paper scissors or 'exit': ")
+    }
+
+    if(input === "exit"){
+        console.log("Have a great day my friend")
+    }
+}
+
+playUntilYouExit() 
