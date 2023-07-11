e4839c13b5a25653a9b21ff53adcf174d4699ca3
Eyub197 <eyubmehmed122@gmail.com>
2023-07-11 11:43:49 +0300

    Finsihed the rules method

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const choice = ["rock", "paper", "scissors"]
+let playerChoice = prompt("Choose rock, paper or scissors").toLowerCase
 
 let getComputerChoice = () => {
     let randomIndex = Math.floor(Math.random() * choice.length) 
     let computerChoice = choice[randomIndex]
-    alert(computerChoice)
+    return computerChoice
 }
 
 
-let rules = (computerChoice , playerChoice) => {
-
+let rules = (computerChoice = getComputerChoice() , plChoice = playerChoice) => {
+    let winner;
+    if(
+    (computerChoice === "rock" && plChoice === "scissors") || 
+    (computerChoice === "paper" &&  plChoice === "rock")   ||
+    (computerChoice === "scissors" && plChoice === "paper")){
+    return `You lost`
+    } else if (computerChoice === plChoice){
+        return "Tie"
+    }else{
+        return "you win"
+    }
 }
 
-getComputerChoice()
